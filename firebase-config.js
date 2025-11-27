1bdd2fee248372d27710c88f0297810a197383ed
surfexcel <160421922+surfexcel-125@users.noreply.github.com>
2025-11-27 20:28:52 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,18 +3,21 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+
+// NOTE: This file only initializes and exports references. No auth side-effects here.
 const firebaseConfig = {
-  apiKey: "AIzaSyCfEZ4gJEMFfwCBq7N4XecRli0qCAThyCE",
-  authDomain: "aries-48190.firebaseapp.com",
-  projectId: "aries-48190",
-  storageBucket: "aries-48190.firebasestorage.app",
-  messagingSenderId: "1030954306592",
-  appId: "1:1030954306592:web:1242fdf52c9b2107424045"
+apiKey: "AIzaSyCfEZ4gJEMFfwCBq7N4XecRli0qCAThyCE",
+authDomain: "aries-48190.firebaseapp.com",
+projectId: "aries-48190",
+storageBucket: "aries-48190.firebasestorage.app",
+messagingSenderId: "1030954306592",
+appId: "1:1030954306592:web:1242fdf52c9b2107424045"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
 
+export { app, auth, db };
